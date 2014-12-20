c0f4bd8382a75621a128fa1860b6a4423aa66308
Allen Hernandez <AllenSH12@users.noreply.github.com>
2014-12-20 02:00:56 -0800

    get hello world working

--- src/table.js
+++ src/table.js
@@ -3,7 +3,7 @@ var React = require('react');
 module.exports = React.createClass({
   render: function() {
     return (
-      <div>test</div>
+      React.createElement('div', null, 'test')
     );
   }
 });
