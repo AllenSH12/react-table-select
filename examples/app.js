0ef3a2f60c59fd3e3cea903768925a9dfceb48ed
Allen Hernandez <AllenSH12@users.noreply.github.com>
2014-12-20 17:00:54 -0800

    move data into example app, add event handlers to show state on internal change and as accessible from the outside

--- examples/app.js
+++ examples/app.js
@@ -1,10 +1,58 @@
 var React = require('react');
 var Table = require('../index');
 
+var data = [
+{
+  "age": "<5",
+  "population": "2704659",
+  "minor": "True"
+},
+{
+  "age": "5-13",
+  "population": "4499890",
+  "minor": "True"
+},
+{
+  "age": "14-17",
+  "population": "2159981",
+  "minor": "True"
+},
+{
+  "age": "18-24",
+  "population": "3853788"
+},
+{
+  "age": "25-44",
+  "population": "14106543"
+},
+{
+  "age": "45-64",
+  "population": "8819342"
+},
+{
+  "age": "≥65",
+  "population": "612463"
+}
+];
+
 var App = React.createClass({
+  handleChange: function(selectedRows) {
+    console.log('From change:');
+    console.log(selectedRows);
+  },
+
+  handleClick: function(e) {
+    console.log('From click:');
+    console.log(this.refs.table.state.selectedRows);
+  },
+
   render: function() {
     return (
-      <Table className="table"/>
+      <div>
+        <button className='btn btn-default' onClick={this.handleClick}>Click Me!</button>
+        <Table className="table" ref="table" data={data} onChange={this.handleChange} />
+      </div>
+
     );
   }
 });
